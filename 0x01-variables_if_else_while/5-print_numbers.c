ea5f6d924072c0504a691c2a419394121e350701
DevKola <koladeayinde@gmail.com>
2023-02-18 11:39:30 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- Prints numer zer to ten.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	for (n = 0; n <= 10; n++)
+		printf("%d\n", n);
+
+	return (0);
+}
