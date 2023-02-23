b0c6a7bdc96392a437df786f872e20109f6393bb
DevKola <koladeayinde@gmail.com>
2023-02-23 11:48:35 -0800

    The problem of distinguishing prime numbers from composite numbers and of resolving the latter into their prime factors is known to be one of the most important and useful in arithmetic

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "main.h"
+#include "math.h"
 
 /**
  * main - frinds and print the pargest prime factor of the
@@ -18,16 +18,16 @@ int main(void)
 		max = 2;
 		num /= 2;
 	}
-	for (i = 3; i <= sqrt(n); i = i + 2)
+	for (i = 3; i <= sqrt(num); i = i + 2)
 	{
-		while (n % i == 0)
+		while (num % i == 0)
 		{
 			max = i;
-			n = n / i;
+			num = num / i;
 		}
 	}
 
-	if (n > 2)
+	if (num > 2)
 		max = num;
 
 	printf("%ld\n", max);
