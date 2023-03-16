51c3d1cd1b1206c9a2eec909a1884ce83873bbfb
DevKola <koladeayinde@gmail.com>
2023-03-16 13:34:21 -0700

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -14,10 +14,10 @@ int i, j;
 	{ printf("Error\n");
 	exit(98); }
 	for (i = 1; i < argc; i++)
-	}
-		for (j = 0; argc[i][j] != '\0'; j++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (argv[i][j] > 57 || argc[i][j] < 48)
+			if (argv[i][j] > 57 || argv[i][j] < 48)
 			{ printf("Error\n");
 			exit(98); }
 		}
